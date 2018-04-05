79a4014384c3f3c6b2ebe80543f069d7340aa8d3
Jack Allen <jackallensemail+bitbit@gmail.com>
2018-04-05 13:53:44 +0100

    init

---
+++ global.js
@@ -0,0 +1,15 @@
+var settings = {
+    "client_id": "O_EChGpxqjLChQ",
+    "client_secret": "H9CWvH1MyixCwpUZeg4VC2OcKCk"
+};
+
+var api = {
+    "reddit_base_url" : "https://www.reddit.com",
+    "reddit_oauth_base_url" : "https://oauth.reddit.com",
+    "reddit_access_token" : "/api/v1/access_token"
+};
+
+module.exports = {
+    settings: settings,
+    api: api
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var axios = require('axios');
+var global = require("../global");
+
+// curl -X POST -d 'grant_type=password&username=jackkallenn&password=nmnmnmnm1' -A "LlamaApp/0.1 by JackkAllenn" --user 'O_EChGpxqjLChQ:H9CWvH1MyixCwpUZeg4VC2OcKCk' https://www.reddit.com/api/v1/access_token
 
 /* GET home page. */
-router.get('/access_token', function (req, res, next) {
-    res.send({title: 'Express'});
+router.post('/access_token', function (req, res, next) {
+
+    axios.request({
+        method: "POST",
+        baseURL: global.api.reddit_base_url,
+        url: global.api.reddit_access_token,
+
+        headers: {
+            "User-Agent": "LlamaApp/0.1 by JackkAllenn",
+            "Authorization" : "Basic " + Buffer.from(global.settings.client_id + ":" + global.settings.client_secret, 'ascii').toString("base64")
+        },
+
+        params: {
+            grant_type: "password",
+            username: req.body.username,
+            password: req.body.password
+        }
+    }).then(function (response) {
+        res.send({
+            access_token : response.data.access_token
+        });
+    }).catch(function (error) {
+        res.send({error: "something bad happened"});
+    })
 });
 
 module.exports = router;
